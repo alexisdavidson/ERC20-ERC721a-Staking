819bdd6724fc0f2ce8d06ea20b48842029c4e5da
Alexis Davidson <alexisdavidson00@gmail.com>
2022-08-20 22:30:23 +0200

    Add staking reward prediction in unit test

--- src/backend/test/NFTStaker.test.js
+++ src/backend/test/NFTStaker.test.js
@@ -1,12 +1,17 @@
 const { expect } = require("chai")
+const helpers = require("@nomicfoundation/hardhat-network-helpers")
 
 const toWei = (num) => ethers.utils.parseEther(num.toString())
-const fromWei = (num) => ethers.utils.formatEther(num)
+const fromWei = (num) => Math.round(ethers.utils.formatEther(num))
 
 describe("NFTStaker", async function() {
-    let deployer, addr1, addr2, nft, token, nftStaker
+    let deployer, addr1, addr2, nft, token, nftStaker, rewardRate
     let teamWallet = "0x90f79bf6eb2c4f870365e785982e1f101e93b906"
     let whitelist = ["0x70997970c51812dc3a010c7d01b50e0d17dc79c8"]
+        
+    let secondsInDay = 86400
+    rewardRate = Math.floor(toWei(5) / secondsInDay).toString()
+    console.log("Reward rate is " + rewardRate.toString() + " per second")
 
     beforeEach(async function() {
         // Get contract factories
@@ -26,12 +31,12 @@ describe("NFTStaker", async function() {
     describe("Deployment", function() {
         it("Should the token initial claim on the staker contract", async function() {
             // Nft Staker contract claims the initial supply
-            expect((await token.balanceOf(nftStaker.address)).toString()).to.equals("222000000000000000000000000");
-            expect((await token.totalSupply()).toString()).to.equals("222000000000000000000000000");
+            expect(fromWei(await token.balanceOf(nftStaker.address))).to.equals(222000000);
+            expect(fromWei(await token.totalSupply())).to.equals(222000000);
             expect((await token.initialSupplyClaimed())).to.equals(true);
             await expect((token.claimInitialSupply())).to.be.revertedWith('Initial supply has already been claimed');
             
-            // expect(await token.rewardRate()).to.equal(5 * 10**uint(rewardsToken.decimals()) / 1 days);
+            expect((await nftStaker.rewardRate()).toString()).to.equal(rewardRate);
         })
     })
 
@@ -42,21 +47,30 @@ describe("NFTStaker", async function() {
             
             // Stake
             await nft.connect(addr1).setApprovalForAll(nftStaker.address, true);
+
             await nftStaker.connect(addr1).stake(333);
+            
+            const blockNumBefore = await ethers.provider.getBlockNumber();
+            const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+            expect(await nftStaker.stakes(addr1.address, 333)).to.equals(blockBefore.timestamp);
+
             expect((await nft.ownerOf(333))).to.equals(nftStaker.address);
             expect((await token.balanceOf(addr1.address))).to.equals(0);
-            expect((await token.balanceOf(nftStaker.address)).toString()).to.equals("222000000000000000000000000");
+            expect(fromWei(await token.balanceOf(nftStaker.address))).to.equals(222000000);
 
-            // Unstake
+            // Unstake after 10 days
+            const tenDays = 10 * 24 * 60 * 60 + 10;
+            await helpers.time.increase(tenDays);
+
             await nftStaker.connect(addr1).unstake(333);
             expect((await nft.ownerOf(333))).to.equals(addr1.address);
 
-            const sevenDays = 7 * 24 * 60 * 60;
-            await ethers.provider.send('evm_increaseTime', [sevenDays]);
+            // Expecting 50 units as reward
+            console.log("Expected Reward: " + fromWei((rewardRate * tenDays).toString()))
+            console.log("Staker actual new balance: " + fromWei(await token.balanceOf(addr1.address)))
 
-            // todo: predict correct reward amount 
-            // expect((await token.balanceOf(addr1.address)).toString()).to.equals((await nftStaker.rewardRate() * sevenDays).toString());
-            // expect((await token.balanceOf(nftStaker.address)).toString()).to.equals("222000000000000000000000000");
+            expect(fromWei(await token.balanceOf(addr1.address))).to.equals(fromWei((rewardRate * tenDays).toString()));
+            expect(fromWei(await token.balanceOf(nftStaker.address))).to.equals(222000000 - fromWei((rewardRate * tenDays).toString()));
         })
     })
 })
