af36395684cd7fc0aade9306a9fd8cf0c4f04356
Alexis Davidson <alexisdavidson00@gmail.com>
2022-08-31 19:35:43 +0200

    Use struct for stakers

--- src/backend/contracts/NFTStaker.sol
+++ src/backend/contracts/NFTStaker.sol
@@ -15,8 +15,13 @@ contract NFTStaker is ERC721Holder, ReentrancyGuard {
     // Reward to be paid out per second
     uint256 public rewardRate;
 
+    struct Staker { 
+        uint256[] tokenIds;
+        uint256[] timestamps;
+    }
+
     // map staker address to stake details (stakes[address][tokenId] = timestamp)
-    mapping(address => mapping(uint256 => uint256)) public stakes;
+    mapping(address => Staker) private stakers;
 
     constructor(address nftAddress, address rewardsTokenAddress) {
         parentNFT = ERC721A(nftAddress);
@@ -27,7 +32,8 @@ contract NFTStaker is ERC721Holder, ReentrancyGuard {
     }
 
     function stake(uint256 _tokenId) public {
-        stakes[msg.sender][_tokenId] = block.timestamp; 
+        stakers[msg.sender].tokenIds.push(_tokenId);
+        stakers[msg.sender].timestamps.push(block.timestamp); 
         parentNFT.safeTransferFrom(msg.sender, address(this), _tokenId);
     } 
 
@@ -35,16 +41,42 @@ contract NFTStaker is ERC721Holder, ReentrancyGuard {
         // Unstake NFT from this smart contract
         parentNFT.safeTransferFrom(address(this), msg.sender, _tokenId);
 
+        uint256 tokenIndex = 0;
+        // Find token Index
+        uint256 tokensLength = stakers[msg.sender].tokenIds.length;
+        for(uint256 i = 0; i < tokensLength; i ++) {
+            if (stakers[msg.sender].tokenIds[i] == _tokenId) {
+                tokenIndex = i;
+                break;
+            }
+        }
+
         // Handout reward depending on the stakingTime
-        uint256 stakingTime = block.timestamp - stakes[msg.sender][_tokenId];
+        uint256 stakingTime = block.timestamp - stakers[msg.sender].timestamps[tokenIndex];
         uint256 reward = stakingTime * rewardRate;
 
         rewardsToken.transfer(msg.sender, reward);
 
-        delete stakes[msg.sender][_tokenId];
+        (stakers[msg.sender].timestamps[tokenIndex], stakers[msg.sender].timestamps[tokensLength - 1]) = (stakers[msg.sender].timestamps[tokensLength - 1], stakers[msg.sender].timestamps[tokenIndex]);
+        stakers[msg.sender].timestamps.pop();
+
+        (stakers[msg.sender].tokenIds[tokenIndex], stakers[msg.sender].tokenIds[tokensLength - 1]) = (stakers[msg.sender].tokenIds[tokensLength - 1], stakers[msg.sender].tokenIds[tokenIndex]);
+        stakers[msg.sender].tokenIds.pop();
     }
 
     function isTokenStaked(uint256 _tokenId) public view returns(bool) {
-        return stakes[msg.sender][_tokenId] > 0;
+        uint256 tokensLength = stakers[msg.sender].tokenIds.length;
+        for(uint256 i = 0; i < tokensLength; i ++) {
+            if (stakers[msg.sender].tokenIds[i] == _tokenId) {
+                return true;
+            }
+        }
+        return false;
+        // return stakers[msg.sender][_tokenId] > 0;
+    }
+    
+    function getStakedTokens(address _user) public view returns (uint256[] memory tokenIds)
+    {
+        return stakers[_user].tokenIds;
     }
 }
