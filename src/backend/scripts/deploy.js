d7679914676c4f35e929178f46a1a1fa2cfbdd06
Alexis Davidson <alexisdavidson00@gmail.com>
2022-09-13 09:21:21 +0200

    Refactor

--- src/backend/scripts/deploy.js
+++ src/backend/scripts/deploy.js
@@ -1,38 +1,41 @@
 // Before deploy:
-// transfer ownership to? (add code)
-// -Wallet address of the team for the 333 NFTs? mainnet: 0x61603b8A09C2Aa8f663B43c22C9ceBeC00FC6FeC
-// team rinkeby test account: 0xCdb34512BD8123110D20852ebEF947275f7fD1Ce
-// -Whitelist addresses?
-// -gelato token name and symbol
-// -gelato nft symbol
+// -Fill whitelist addresses with correct data!
+// -Team Wallet mainnet: 0x61603b8A09C2Aa8f663B43c22C9ceBeC00FC6FeC
+// -Team Wallet rinkeby: 0xCdb34512BD8123110D20852ebEF947275f7fD1Ce
 
 async function main() {
-
   const [deployer] = await ethers.getSigners();
 
   console.log("Deploying contracts with the account:", deployer.address);
   console.log("Account balance:", (await deployer.getBalance()).toString());
 
-  // const teamWallet = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc"; // (localhost) Fill with correct input before deploy!
-  const teamWallet = "0xCdb34512BD8123110D20852ebEF947275f7fD1Ce"; // (rinkeby) Fill with correct input before deploy!
-  // Deploy contracts
+  // Fill with correct data and uncomment the correct network before deploy!
+  // const teamWallet = "0x3c44cdddb6a900fa2b585dd299e03d12fa4293bc"; // localhost
+  const teamWallet = "0xCdb34512BD8123110D20852ebEF947275f7fD1Ce"; // rinkeby
+  // const teamWallet = "0x61603b8A09C2Aa8f663B43c22C9ceBeC00FC6FeC"; // mainnet
+  
+  // Fill with correct data and uncomment the correct network before deploy!
+  // const whitelistAddresses = [teamWallet, "0x70997970c51812dc3a010c7d01b50e0d17dc79c8"] // localhost
+  const whitelistAddresses = [teamWallet, "0x1e85F8DAd89e993A2c290B846F48B62B151da8af", "0xA8095a8AB93D7cad255248D1D685D4a9F9eF2621", "0x1354075Cd28774e7D952F3Bb786F17959d8C6B61"] // rinkeby
+  // const whitelistAddresses = [teamWallet] // mainnet
+  
   const NFT = await ethers.getContractFactory("NFT");
   const Token = await ethers.getContractFactory("Token");
   const NFTStaker = await ethers.getContractFactory("NFTStaker");
-  // const nft = await NFT.deploy(teamAddress, [teamAddress, "0x70997970c51812dc3a010c7d01b50e0d17dc79c8"]); // (localhost) Fill with correct input before deploy!
-  const nft = await NFT.deploy(teamWallet, [teamWallet, "0x1e85F8DAd89e993A2c290B846F48B62B151da8af", "0xA8095a8AB93D7cad255248D1D685D4a9F9eF2621", "0x1354075Cd28774e7D952F3Bb786F17959d8C6B61"]); // (rinkeby) Fill with correct input before deploy!
+  const nft = await NFT.deploy(teamWallet, whitelistAddresses);
   console.log("NFT contract address", nft.address)
   const nftStaker = await NFTStaker.deploy(nft.address);
   console.log("NFTStaker contract address", nftStaker.address)
   const token = await Token.deploy([nftStaker.address, teamWallet], [73000000, 149000000]);
   console.log("Token contract address", token.address)
   await nftStaker.setOwnerAndTokenAddress(teamWallet, token.address);
-  console.log("setTokenAddress")
+  console.log("setOwnerAndTokenAddress call done")
   
-  // For each contract, pass the deployed contract and name to this function to save a copy of the contract ABI and address to the front end.
   saveFrontendFiles(nft, "NFT");
   saveFrontendFiles(token, "Token");
   saveFrontendFiles(nftStaker, "NFTStaker");
+
+  console.log("Frontend files saved")
 }
 
 function saveFrontendFiles(contract, name) {
